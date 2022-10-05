92c623a2fd3ced8a6cb072c16dffffb58bbe123a
Wietse Venema <356014+wietsevenema@users.noreply.github.com>
2022-10-05 22:28:05 +0200

    Make sure ADC works

--- base-containers/base/docker-entrypoint.sh
+++ base-containers/base/docker-entrypoint.sh
@@ -13,22 +13,26 @@ gcloud_init() {
     if [ -n "${INSTRUQT_GCP_PROJECTS}" ]; then
         PROJECTS=("${INSTRUQT_GCP_PROJECTS//,/ }")
 
-        # load all credentials into gcloud
-        for PROJECT in "${PROJECTS[@]}"; do
-            TMP_FILE=$(mktemp)
-            SERVICE_ACCOUNT_KEY="INSTRUQT_GCP_PROJECT_${PROJECT}_SERVICE_ACCOUNT_KEY"
-            base64 -d <(echo "${!SERVICE_ACCOUNT_KEY}") > "$TMP_FILE"
-            gcloud auth activate-service-account --key-file="$TMP_FILE"
-            rm "$TMP_FILE"
-        done
+        # Load all credentials of the FIRST project into gcloud
+        TMP_FILE=/root/.config/gcloud/default_credentials.json
+        SERVICE_ACCOUNT_KEY="INSTRUQT_GCP_PROJECT_${PROJECTS[0]}_SERVICE_ACCOUNT_KEY"
+        base64 -d <(echo "${!SERVICE_ACCOUNT_KEY}") > "$TMP_FILE"
+        gcloud auth activate-service-account --key-file="$TMP_FILE"
 
-        # activate service account for first project
+        PROJECT_ID_VAR="INSTRUQT_GCP_PROJECT_${PROJECTS[0]}_PROJECT_ID"
+
+        # Configure project
+        gcloud config set project "${!PROJECT_ID_VAR}"
+
+        # Activate application default credentials
+        echo "export GOOGLE_APPLICATION_CREDENTIALS=/root/.config/gcloud/default_credentials.json" >> /etc/profile.d/google-adc.sh
+        echo "export GOOGLE_CLOUD_PROJECT=${!PROJECT_ID_VAR}" >> /etc/profile.d/google-adc.sh
+
+        # Activate service account for first project
         SERVICE_ACCOUNT_EMAIL="INSTRUQT_GCP_PROJECT_${PROJECTS[0]}_SERVICE_ACCOUNT_EMAIL"
         gcloud config set account "${!SERVICE_ACCOUNT_EMAIL}"
 
-        # configure project
-        PROJECT_ID="INSTRUQT_GCP_PROJECT_${PROJECTS[0]}_PROJECT_ID"
-        gcloud config set project "${!PROJECT_ID}"
+        
     fi
 }
 
